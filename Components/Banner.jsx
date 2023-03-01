e2c8e6cb66221598cdedc960250fd719071d218e
Areej Amjad <areej.amjad14@gmail.com>
2023-03-01 10:33:08 +0500

    Update Banner.jsx

--- Components/Banner.jsx
+++ Components/Banner.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import banner from '../public/imgs/page/homepage8/ads1.png'
 const Banner = () => {
   return (
     <div className="section-box mt-30 mb-50">
       <div className="container">
-        <div className="box-ads-2 box-ads-3"> <img src={`../imgs/page/homepage8/ads1.png`} alt="Ecom"/>
+        <div className="box-ads-2 box-ads-3"> <img src={`${banner.src}`} alt="Ecom"/>
           <div className="ads-info">
             <h6 className="color-white mb-5">Best Book Colletion</h6>
             <h3 className="color-white mb-15">Let’s Change Things Inspire Book</h3>
